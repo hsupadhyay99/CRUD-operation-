7d6761795a8af89693f5ed3127ac4a2be07eeb56
Hari Shankar Upadhyay <hsupadhyay99@gmail.com>
2021-07-01 17:10:06 +0530

    validater

--- crud-operation/src/main/java/com/hari/upadhyay/entity/Note.java
+++ crud-operation/src/main/java/com/hari/upadhyay/entity/Note.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "notes")
@@ -12,9 +13,11 @@ public class Note {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+    
+	@NotBlank(message = "title is mandatory")
 	private String title;
 
+	
 	private String content;
 
 	
